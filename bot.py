2396c91e908a92c68909affb49ba0bee15b62c47
speed <43330808+jsinitx4@users.noreply.github.com>
2019-03-03 13:03:38 -0500

    fix a lot

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
+import asyncio
+import youtube_dl
 import random
+
+import sys, traceback
 
 from discord.ext import commands
 from assets.list import *
@@ -7,51 +11,37 @@ prefix = "$"
 bot = commands.Bot(command_prefix=prefix)
 bot.remove_command('help')
 
-extensions = [
-    "commands.useful_stuff",
-    "commands.music"
-    ]
-
 players = {}
 
 @bot.event
 async def on_ready():
     print("successful startup")
     await bot.change_presence(game=discord.Game(name='fortnite 2'))
-    for extension in extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print(f"Failed to load extension {extension}. Reason: {e}")
 
 @bot.command(pass_context=True)
 async def say(ctx, *, content:str):
-    await ctx.send(content)
+    await bot.say(content)
 
 @bot.command(name="8ball")
 async def ball(ctx):
     okbuddy = random.choice(differentball)
-    await ctx.send(okbuddy)
-
-@bot.command(pass_context=True)
-async def dev(ctx): #this was all a mistake
-    await ctx.send("my daddy is `speed#5496`")
+    await bot.say(okbuddy)
 
 @bot.command(pass_context=True)
 async def add(ctx, a: int, b: int):
-    await ctx.send(a + b)
+    await bot.say(a + b)
 
 @bot.command(pass_context=True)
 async def multiply(ctx, a: int, b: int):
-    await ctx.send(a * b)
+    await bot.say(a * b)
 
 @bot.command(pass_context=True)
 async def subtract(ctx, a: int, b: int):
-    await ctx.send(a - b)
+    await bot.say(a - b)
 
 @bot.command(pass_context=True)
 async def divide(ctx, a: int, b: int):
-    await ctx.send(a / b)
+    await bot.say(a / b)
 
 @bot.command(pass_context=True)
 async def join(ctx):
@@ -65,55 +55,44 @@ async def leave(ctx):
     await voice_client.disconnect()
 
 @bot.command(pass_context=True)
-async def pause(ctx):
-    id = ctx.message.server.id
-    players[id].pause()
-    await ctx.send("ok")
-
-@commands.command(pass_context=True)
-async def play( ctx, url):
+async def play(ctx, url):
     server = ctx.message.server
     voice_client = bot.voice_client_in(server)
     player = await voice_client.create_ytdl_player(url)
     players[server.id] = player
     player.start()
+    await bot.say("now playing")
+
+@bot.command(pass_context=True)
+async def pause(ctx):
+    id = ctx.message.server.id
+    players[id].pause()
+    await bot.say("ok")
 
 @bot.command(pass_context=True)
 async def resume(ctx):
     id = ctx.message.server.id
     players[id].resume()
-    await ctx.send("hi welcome back")
+    await bot.say("hi welcome back")
 
 @bot.command(pass_context=True)
 async def math(ctx): 
-    await ctx.send("`subcommands` $add, $divide, $subtract, $multiply")
+    await bot.say("`subcommands` $add, $divide, $subtract, $multiply")
 
 @bot.command(pass_context=True)
 async def invite(ctx): 
-    await ctx.send("https://discordapp.com/api/oauth2/authorize?client_id=490953980361441281&permissions=8&scope=bot")
+    await bot.say("https://discordapp.com/api/oauth2/authorize?client_id=490953980361441281&permissions=8&scope=bot")
+
+@bot.command(pass_context=True)
+async def dev(ctx): 
+    await bot.say("`speed#3413`")
 
 @bot.command(pass_context=True)
 async def help(ctx):
     embed=discord.Embed(title="help", description="thank you pylint, very cool!", color=0x00FFDD)
     embed.add_field(name="useful stuff", value="`$say, $dev, $math, $8ball, $invite`", inline= True)
     embed.add_field(name="useless stuff", value="`nothing useless yet`", inline= True)
-    embed.add_field(name="music", value="`$join, $leave, $play, $pause, $leave`")
-    await ctx.send(embed=embed)
-
-@bot.command(pass_context=True)
-async def load(ctx, extension: str):
-    try:
-        bot.load_extension(extension)
-        ctx.send("loaded {}".format(extension))
-    except Exception as error:
-        ctx.send("failed to load extension {} [{}]".format(extension, error))
-
-@bot.command(pass_context=True)
-async def unload(ctx, extension: str):
-    try:
-        bot.unload_extension(extension)
-        ctx.send("unloaded {}".format(extension))
-    except Exception as error:
-        ctx.send("failed to unload extension {} [{}]".format(extension, error))
-
+    embed.add_field(name="music", value="`$join, $leave, $play, $pause, $resume`")
+    await bot.say(embed=embed)
+            
 bot.run("TOKEN")
